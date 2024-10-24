c7ca639a80cbd86e5ee7a55d0f7635059d858839
Chris Fisher <chris.i.fisher@gmail.com>
2024-10-24 13:08:58 +1300

    Fix define auth challenge lambda

--- lambdas/define-auth-challenge.ts
+++ lambdas/define-auth-challenge.ts
@@ -3,22 +3,18 @@ import { DefineAuthChallengeTriggerHandler } from "aws-lambda";
 export const handler: DefineAuthChallengeTriggerHandler = async (event) => {
   const { session } = event.request;
 
-  console.log(session);
-
   if (session.length === 1 && session[0].challengeName === "SRP_A") {
     event.response.issueTokens = false;
     event.response.failAuthentication = false;
     event.response.challengeName = "PASSWORD_VERIFIER";
-  } else if (
-    session.length === 2 &&
-    session[1].challengeName === "PASSWORD_VERIFIER"
-  ) {
+  } else if (session.length === 2 && session[1].challengeName === "PASSWORD_VERIFIER") {
     event.response.issueTokens = false;
     event.response.failAuthentication = false;
     event.response.challengeName = "CUSTOM_CHALLENGE";
   } else if (
     session.length === 3 &&
-    session[2].challengeName === "CUSTOM_CHALLENGE"
+    session[2].challengeName === "CUSTOM_CHALLENGE" &&
+    session[2].challengeResult === true
   ) {
     event.response.issueTokens = true;
     event.response.failAuthentication = false;
